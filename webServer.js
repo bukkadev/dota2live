af9200946955c2ad61f64c82f670a35ae5b4bbb4
natoshi <bukka96@gmail.com>
2014-06-15 23:35:26 +0200

    Added Tower State info and search by name fix #2

--- webServer.js
+++ webServer.js
@@ -19,7 +19,22 @@ MongoClient.connect("mongodb://localhost:27017/dota2", function(err, db) {
 	Collections = {};
 	Collections.Games = db.collection("games");
 
+	var steamId = "";
+	var heroId = "";
+	var steamName = "";
+
+	app.use(function(request, response, next) {
+		response.locals.steamId = steamId;
+		response.locals.heroId = heroId;
+		response.locals.steamName = steamName;
+		next();
+	});
+
 	app.get('/', function(req, res){
+		steamId = "";
+		heroId = "";
+		steamName = "";
+
 		a = url.parse(req.url, true);
 		var page = 1;
 		if (a.query.page) {
@@ -94,6 +109,28 @@ MongoClient.connect("mongodb://localhost:27017/dota2", function(err, db) {
 	}
 
 
+	function findByName(data, name){
+		var pages = data;
+
+		for (var i = 0; i < pages.length; i++) {
+			for (var q = 0; q < pages[i].games.length; q++) {
+				for (var w = 0; w < pages[i].games[q].goodPlayers.length; w++) {
+
+					if (pages[i].games[q].goodPlayers[w].name == name ) {
+						return pages[i].games[q];
+					};
+
+					if (pages[i].games[q].badPlayers[w]) {
+						if (pages[i].games[q].badPlayers[w].name == name ) {
+							return pages[i].games[q];
+						}
+					};
+				};
+			};
+		};
+	}
+
+
 	app.get('/search/', function(req, res){
 		Collections.Games.find().toArray(function(err, pages) {
 			if (err) {
@@ -102,10 +139,6 @@ MongoClient.connect("mongodb://localhost:27017/dota2", function(err, db) {
 
 			var games = pages[pages.length - 1].pages;
 
-			steamId = "";
-			heroId = "";
-			name = "";
-
 			if (req.query.steamId) {
 				gamesFound = [findBySteamId(games, req.query.steamId)];
 				steamId = req.query.steamId;
@@ -113,8 +146,8 @@ MongoClient.connect("mongodb://localhost:27017/dota2", function(err, db) {
 				gamesFound = findByHeroId(games, req.query.heroId);
 				heroId = req.query.heroId;
 			}else if(req.query.name){
-				gamesFound = findByName(games, req.query.name);
-				name = req.query.name;
+				gamesFound = [findByName(games, req.query.name)];
+				steamName = req.query.name;
 			}
 			
 			gamesFound = [{"gamesFound" : gamesFound.length}, gamesFound];
@@ -126,7 +159,7 @@ MongoClient.connect("mongodb://localhost:27017/dota2", function(err, db) {
 				);
 
 			}else{
-				res.render("index", {gamesFound: gamesFound, pages:pages, steamId: steamId, heroId: heroId, name : name}, function(err, html) {
+				res.render("index", {gamesFound: gamesFound, pages:pages, steamId: steamId, heroId: heroId, steamName : steamName}, function(err, html) {
 					if (err) {
 						console.error(err);
 					};
